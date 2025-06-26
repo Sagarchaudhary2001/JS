4f1503328b49197586ed2154f92f5b4b09ed50a7
Sagarchaudhary2001 <sagarchaudhary55251@gmail.com>
2025-06-26 12:38:31 +0000

    about constant and datatype

---
+++ .devcontainer/const_var.js
@@ -0,0 +1,6 @@
+const accountId=130101
+var accountEmail="sagarchaudhary55251@gmail.com"
+let accountPassword="12345"
+accountName="Sagarchaudhary2001"
+accountName="sagar"
+console.table([accountEmail,accountId,accountName,accountPassword])
---
+++ .devcontainer/datatype.js
@@ -0,0 +1,9 @@
+let score="10"
+console.log(typeof score);
+
+let value=Number(score)
+console.log(typeof value);
+
+let key=undefined;
+console.log(typeof key);
+
